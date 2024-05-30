311327fbfe3c9807e125384803aea34f44ce3f08
Quân Huỳnh Anh <quan.huynhanh@kingfoodmart.net>
2024-05-30 08:47:49 +0700

    add fullname input

--- week-1/exercise-2/frontend/src/app/signup/signup.component.ts
+++ week-1/exercise-2/frontend/src/app/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import {
     AbstractControl,
@@ -5,14 +6,12 @@ import {
     FormGroup,
     FormSubmittedEvent,
     ReactiveFormsModule,
-    Validators,
-    ValueChangeEvent,
+    Validators
 } from '@angular/forms';
-import {} from '@angular/material/input';
-import AuthService from '../auth/auth.service';
+import { } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { catchError, of } from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
+import AuthService from '../auth/auth.service';
 
 function passwordConfirming(control: AbstractControl) {
     if (control?.value.password !== control?.value.confirmPassword) {
@@ -38,6 +37,7 @@ export class SignupComponent implements OnInit {
     signUpForm = new FormGroup(
         {
             username: new FormControl('', [Validators.required]),
+            fullName: new FormControl('', [Validators.required]),
             password: new FormControl('', [Validators.required]),
             confirmPassword: new FormControl('', [Validators.required]),
         },
@@ -58,6 +58,10 @@ export class SignupComponent implements OnInit {
         return this.signUpForm.get('username');
     }
 
+    get fullName() {
+        return this.signUpForm.get('fullName');
+    }
+
     get password() {
         return this.signUpForm.get('password');
     }
@@ -74,7 +78,7 @@ export class SignupComponent implements OnInit {
         const ob = this.authService.register({
             username: this.username?.value!,
             password: this.password?.value!,
-            full_name: 'quan',
+            full_name: this.fullName?.value!,
         });
         ob.pipe(
             catchError((error) => {
