a02ac4decc514362073c35fbfbfd99bfe145c6bb
Ryuukaiser <gauvu107@gmail.com>
2024-06-04 23:59:49 +0700

    get layout

--- projects/project-1/fengshenlist/src/views/HomeView.vue
+++ projects/project-1/fengshenlist/src/views/HomeView.vue
@@ -1,9 +1,80 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Card from 'primevue/card';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex gap-2 flex-col">
+     <div class="flex gap-2 items-center r">
+     <Card style="width: 25rem; overflow: hidden">
+      <template #header>
+      </template>
+      <template #title>Advanced Card</template>
+      <template #subtitle>Card subtitle</template>
+      <template #content>
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+      </template>
+      <template #footer>
+        <div class="flex gap-3 mt-1">
+          <Button label="Cancel" severity="secondary" outlined class="w-full" />
+          <Button label="Save" class="w-full" />
+        </div>
+      </template>
+    </Card>
+      <Card style="width: 25rem; overflow: hidden">
+      <template #header>
+      </template>
+      <template #title>Advanced Card</template>
+      <template #subtitle>Card subtitle</template>
+      <template #content>
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+      </template>
+      <template #footer>
+        <div class="flex gap-3 mt-1">
+          <Button label="Cancel" severity="secondary" outlined class="w-full" />
+          <Button label="Save" class="w-full" />
+        </div>
+      </template>
+    </Card>
+      <Card style="width: 25rem; overflow: hidden">
+      <template #header>
+      </template>
+      <template #title>Advanced Card</template>
+      <template #subtitle>Card subtitle</template>
+      <template #content>
+        <p class="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+      </template>
+      <template #footer>
+        <div class="flex gap-3 mt-1">
+          <Button label="Cancel" severity="secondary" outlined class="w-full" />
+          <Button label="Save" class="w-full" />
+        </div>
+      </template>
+    </Card>
+  </div>
+  <div>
+  <DataTable :value="products" tableStyle="min-width: 50rem">
+      <Column field="code" header="Code"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="category" header="Category"></Column>
+      <Column field="quantity" header="Quantity"></Column>
+  </DataTable>
+</div>
   </main>
 </template>
